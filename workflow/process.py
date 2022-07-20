969d36c0fd194536f9c074108c1732c39836c262
zhengxiuhang <zhengxiuhang@gmail.com>
2022-07-20 16:45:29 +0800

    update support millisecond

--- workflow/process.py
+++ workflow/process.py
@@ -27,6 +27,8 @@ def parse_query_value(query_str):
         else:
             # Parse datetime string or timestamp
             try:
+                if str(query_str).isdigit() and len(query_str) == 13:
+                    query_str = int(query_str) / 1000
                 d = epoch(float(query_str))
             except ValueError:
                 d = parse(str(query_str), get_timezone())
